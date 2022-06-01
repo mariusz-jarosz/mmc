288165d53d437dc9b028c6e179a5662776f84b3f
Mariusz Jarosz <mariusz@jarosz.eu>
2022-06-01 13:37:21 +0200

    II.65

--- II/57/js/main.js
+++ II/57/js/main.js
@@ -3,4 +3,7 @@ let eat = 'słonine'
 let age = ''
 age = 44
 
+const table = ['kjhkj', 'lkhjlk']
+console.log(table[0])
+
 console.log(`Mam na imię ${name}, mam ${age} wiosny i lubię ${eat}.`)
---
+++ II/65/js/main.js
@@ -0,0 +1,13 @@
+let score = 15%2
+console.log(score)
+
+let num1 = 8
+num1++
+num1++
+console.log(num1)
+
+let x = 5
+let y = 10
+
+console.log(x*=y)
+console.log(x/=y)
